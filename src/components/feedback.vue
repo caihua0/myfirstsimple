62439e017adb483bfbe5a59005509f239ea1be37
caihua0 <caihua_0@163.com>
2017-07-18 21:32:52 +0800

    20170717

--- src/components/feedback.vue
+++ src/components/feedback.vue
@@ -6,35 +6,43 @@
 			<div class="body" >
 				<!-- 您的姓名 -->
 				<div class="group">
-					<i class="fa-name"></i>
+					<i class="fa fa-user-o"></i>
 					<input type="text" name="name" placeholder="姓名" v-model="feedback.name">
 				</div>
 
 				<!-- 您的联系方式 -->
 				<div class="group">
-					<input type="test" name="phone" placeholder="联系方式" v-model="feedback.phone">
+					<i class="fa fa-mobile"></i>
+					<input type="text" name="phone" placeholder="联系方式" v-model="feedback.phone">
 				</div>
 				<!-- QQ或邮箱 -->
 				<div class="group">
+					<i class="fa fa-envelope-o"></i>
 					<input type="text" name="info" placeholder="QQ/Email" v-model="feedback.info">
 				</div>
 				<!-- 您的母校&班级 -->
 				<div class="group">
+					<i class="fa fa-mortar-board"></i>
 					<input type="text" name="school" placeholder="母校" v-model="feedback.school">
 				</div>
 				<!-- 您的毕业年份，大家共度的在校时间 -->
 				<div class="group">
+					<i class="fa fa-calendar"></i>
 					<input type="text" name="graduation" placeholder="在校时间" v-model="feedback.graduation">
 				</div>
 				<!-- 预计加入人数 -->
 				<div class="group">
+					<i class="fa fa-child"></i>
 					<input type="text" name="count" placeholder="人数" v-model="feedback.count">
 				</div>
 				<!-- 是否有意向地点，出行计划 -->
 				<div class="group">
-					<input type="text" name="memo" placeholder="意向及建议" v-model="feedback.memo">
+					<i class="fa fa-drivers-license-o"></i>
+					<textarea rows="5" name="memo" placeholder="意向及建议" v-model="feedback.memo"></textarea>
 				</div>
-				<button type="button" class="login">提交</button>
+			</div>
+			<div class="gbutton">
+				<button type="button" class="button">提交</button>
 			</div>
 		</div>
 	</div>
@@ -54,7 +62,6 @@ export default {
 
 <style>
 *{
-
 	box-sizing: border-box;
 }
 .container{
@@ -85,10 +92,12 @@ export default {
     border-bottom: 1px solid transparent;
 }
 .pannel>.body{
-	padding: 20px;
-	margin-bottom: 15px;
+	padding: 20px 20px 10px 20px;
+	/*margin-bottom: 15px;*/
 }
 input{
+/*	box-shadow: none !important;
+	    margin: 0;*/
 	display: block;
     /*color: #999999;*/
 	width: 100%;
@@ -103,6 +112,23 @@ input{
 	/*box-shadow: none !important;*/
     transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
 }
+textarea{
+	width: 100%;
+	display: block;
+	height: auto;
+	padding: 6px 12px;
+    font-size: 14px;
+	border: 1px solid #e5e5e5;
+	background-color: #fff;
+    background-image: none;
+	line-height: 1.42857143;
+	transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+}
+input[type="text"]:focus,textarea:focus{
+	border-color: #999999;
+    box-shadow: none;
+    outline: 0 none;
+} 
 .group{
 	position: relative;
 }
@@ -116,19 +142,20 @@ input{
     font-size: 16px;
     text-align: center;
 }
-.fa-name:before {
-
-/* 
-	color: #999999;
-    display: block;
-    position: absolute;
-    margin: 10px 2px 4px 10px;
-    width: 16px;
-    height: 16px;
-    font-size: 16px;
-    text-align: center;*/
-
-    content: "\f007";
+.gbutton{
+	padding: 20px;
+    background: #fafafa;
+    border-bottom-right-radius: 4px !important;
+    border-bottom-left-radius: 4px !important;
+    text-align: right;
+}
+.button{
+	color: #ffffff;
+    border: 1px solid #1f68b1;
+    background-color: #428bca;
+    padding: 6px 12px;
+    font-size: 14px;
+    outline: none !important;
 }
 
 
