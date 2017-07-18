70991ca758c81a042959e8d8031248b185b44901
caihua0 <caihua_0@163.com>
2017-07-18 08:09:04 +0800

    feedback
    add feedback

---
+++ src/components/feedback.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container" name="login">
+		<h1></h1>
+		<h2>{{msg}}</h2>
+		<div class="form" >
+			您的姓名<input type="text" name="name" placeholder="">
+			您的联系方式<input type="test" name="phone" placeholder="">
+			QQ或邮箱<input type="text" name="info" placeholder="">
+			您的母校&班级<input type="text" name="school" placeholder="">
+			您的毕业年份，大家共度的在校时间<input type="text" name="graduation" placeholder="">
+			预计加入人数<input type="text" name="count" placeholder="">
+			是否有意向地点，出行计划<input type="text" name="memo" placeholder="">
+			<button type="button" class="login">提交</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to home'
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import XmlTool from './components/xmltool.vue'
 import login from './components/login.vue'
+import feedback from './components/feedback.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 // import xlsxx from 'xlsx'
@@ -13,7 +14,7 @@ Vue.use(VueResource)
 //HTML5 FileReader API 有两个方法可以读取本地文件 readAsBinaryString 和 readAsArrayBuffer, 默认rABS为true，也就是使用readAsBinaryString
 var v = new Vue({
   el: '#app',
-  render: h => h(XmlTool)
+  render: h => h(feedback)
   // render:h=>h(XmlTool)
 })
 
